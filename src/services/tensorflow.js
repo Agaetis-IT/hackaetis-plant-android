d8e5929eff2bd3042d02b4c070627c6c134b9042
Julien Usson <usson.julien@gmail.com>
2018-03-05 16:42:07 +0100

    :sparkles: pokemon tf test

--- src/services/tensorflow.js
+++ src/services/tensorflow.js
@@ -1,15 +1,19 @@
 import { TensorFlow } from 'react-native-tensorflow'
-import graph from '../training/frozen_inference_graph.pb'
+import graph from '../training/pokemon.pb'
 
 export async function imageclassifier() {
+  console.tron.log('start')
   const tf = new TensorFlow(graph)
+  console.tron.log('start')
   await tf.feed({
     data: [1, 2, 3],
     dtype: 'int64',
     name: 'inputName',
-    shape: [1, 2, 4],
+    shape: [1, 3],
   })
+  console.tron.log('start')
   await tf.run(['outputNames'])
+  console.tron.log('start')
   const output = await tf.fetch('outputName')
   // eslint-disable-next-line no-console
   console.tron.log(output)
